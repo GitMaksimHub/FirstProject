c1b83b8b4839ce1b961d25515f0c60d22f0548ac
Maksim <macdacin2345@gmail.com>
2024-02-29 15:54:08 +0000

    Закомментировал строку в urlsforinclude

--- mainpageapp/urlsforinclude.py
+++ mainpageapp/urlsforinclude.py
@@ -22,7 +22,7 @@ from . import views
 
 urlpatterns = [
     path("wayone", views.wayone, name="wayone"),
-    path("waytwo/<slug:slug_id>", views.waytwo, name="waytwo")
+    #path("waytwo/<slug:slug_id>", views.waytwo, name="waytwo")
 ]
 
 
